f55893d87730a25607fa7d3bea89cc8c8ff8ed84
litoilinop <litoilinop@gmail.com>
2023-02-21 22:04:33 +0100

    link id

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -18,7 +18,17 @@ const ProductCard = (props) => {
           location.pathname === "/product" ? `gr-${grid}` : "col-3"
         } `}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          {" "}
           <div className="wishlist-icon position-absolute">
             <button className="border-0 bg-transparent">
               <img src={wish} alt="Favoris" />
@@ -66,7 +76,17 @@ const ProductCard = (props) => {
           location.pathname === "/product" ? `gr-${grid}` : "col-3"
         } `}
       >
-        <Link className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          {" "}
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src={wish} alt="Favoris" />
